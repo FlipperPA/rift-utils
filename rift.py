8c7b50520be1e5c5f01ff746988cf040bf0e812c
Allen, Timothy <tallen@wharton.upenn.edu>
2018-11-22 10:12:15 -0500

    first commit

---
+++ rift.py
@@ -0,0 +1,41 @@
+import smtplib
+
+import requests
+
+
+sender = 'tallen@wharton.upenn.edu'
+receivers = ['6107217758@vtext.com']
+
+events = [
+    'Critical Mass',
+    'Fear From Freedom',
+    'Inner Maelstrom',
+    'Macabre Feast',
+    'Melting Point',
+    'Rude Awakening',
+    'Under the Table',
+    'Thorns Everywhere',
+    'Why So Sad',
+    'Boiling Ambition',
+    'Scuttled Dreams',
+    'Dreams of Blood and Bone',
+    'Chilled to the Bone',
+    'The Snow Pack',
+    'Hunger of the Deep',
+    'Fortress Defense',
+    'Champions of the Firestorm',
+    'The Craft of Conflict',
+]
+
+r = requests.get("https://rift.events/na/en_US.html")
+
+found_events = []
+for event in events:
+    if event in r.text:
+        found_events.append(event)
+
+if len(found_events):
+    events = ', '.join(found_events)
+    print(events)
+    smtp = smtplib.SMTP('localhost')
+    smtp.sendmail(sender, receivers, f"{events}\n\nhttps://rift.events/na/en_US.html")
