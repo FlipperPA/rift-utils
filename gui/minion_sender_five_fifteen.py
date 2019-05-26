e4bf3c976a8e4735d058a601fab541a47fd920e7
Allen, Timothy <tallen@wharton.upenn.edu>
2019-05-26 07:14:45 -0400

    Add 5/15 minion sender script.

---
+++ gui/minion_sender_five_fifteen.py
@@ -0,0 +1,18 @@
+from time import sleep
+
+import pyautogui
+
+for x in range(0, 9999999999):
+    pyautogui.moveTo(298, 681)
+    for x in range(0, 20):
+        pyautogui.click()
+        sleep(0.4)
+    sleep(293)
+
+
+# Chloro Fast Heal
+
+for x in range(1, 352):
+    pyautogui.moveTo(1843, 509)
+    pyautogui.click()
+    sleep(1)
