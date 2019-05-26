4307421c51a20dd5c02d358c9af0814dfc20e8ff
Allen, Timothy <tallen@wharton.upenn.edu>
2019-05-26 12:44:27 -0400

    Tweaking settings.

--- gui/collect_motes_shard_hop.py
+++ gui/collect_motes_shard_hop.py
@@ -2,6 +2,7 @@ from time import sleep
 
 import pyautogui
 
+
 RANGED = (1091, 823)
 AE = (1247, 1016)
 MINIONS = (298, 677)
@@ -32,6 +33,7 @@ def action(coords, sleep_secs=1, repeat=1):
 def shard_hop(shard="Deepwood"):
     # Portrait
     pyautogui.moveTo(896, 118)
+    pyautogui.click()
     pyautogui.rightClick()
     sleep(0.5)
     pyautogui.moveTo(1063, 270)
@@ -44,6 +46,25 @@ def shard_hop(shard="Deepwood"):
 current_shard = "Deepwood"
 
 for x in range(0, 999999):
+    shard_hop(current_shard)
+
+    print(f"Shard hopping to {current_shard}...")
+    if(current_shard == "Deepwood"):
+        current_shard = "Faeblight"
+    elif(current_shard == "Faeblight"):
+        current_shard = "Greybriar"
+    elif(current_shard == "Greybriar"):
+        current_shard = "Haliol"
+    elif(current_shard == "Haliol"):
+        current_shard = "Laethys"
+    elif(current_shard == "Laethys"):
+        current_shard = "Seastone"
+    elif(current_shard == "Seastone"):
+        current_shard = "Wolfbane"
+    else:
+        current_shard = "Deepwood"
+    sleep(10)
+
     for five_min_loop in range(0, 12):
         print("In five minute loop...")
         for x in range(1, 3):
@@ -61,22 +82,3 @@ for x in range(0, 999999):
 
         action(HEAL_BIG, 1, 3)
         action(MINIONS, 0.3, 6)
-
-    shard_hop(current_shard)
-
-    if(current_shard == "Deepwood"):
-        current_shard = "Faeblight"
-    elif(current_shard == "Faeblight"):
-        current_shard = "Greybriar"
-    elif(current_shard == "Greybriar"):
-        current_shard = "Haliol"
-    elif(current_shard == "Haliol"):
-        current_shard = "Laethys"
-    elif(current_shard == "Laethys"):
-        current_shard = "Seastone"
-    elif(current_shard == "Seastone"):
-        current_shard = "Wolfbane"
-    else:
-        current_shard = "Deepwood"
-
-    sleep(10)
